f67058b1adf41e9d2e8f09018d3360a36bcf4161
Noam <niam.k@compie.co.il>
2024-01-21 13:56:14 +0200

    added QrcodeStream

--- pages/scan.vue
+++ pages/scan.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import {QrcodeStream} from 'vue-qrcode-reader';
 
+const stopCamera = ref(false);
+
+const onDecode = (result: string) => {
+  alert(result);
+  stopCamera.value = true;
+};
 </script>
 
 <template>
 <h1>scan</h1>
+
+  <QrcodeStream
+    :debug="false"
+    :stop="stopCamera"
+    @decode="onDecode"
+    style="width: 100%;"
+  />
 </template>
 
 <style scoped>
